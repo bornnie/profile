ee4398e42ed8cfffb920f51e8801b834fb56ac52
bornnie <bornwelljames@gmail.com>
2021-11-13 17:31:49 +0200

    modified db setup inapp.js file

--- app.js
+++ app.js
@@ -4,8 +4,16 @@ require('dotenv').config();
 const app = express();
 
 //DB Setup
-mongoose.connect(process.env.CONN);
-mongoose.connection.once('open',()=>console.log(`Connection Established`)).on('error', err=>console.log(`Connection Error:${err}`));
+mongoose.connect(process.env.CONN,{
+				useNewUrlParser:true,
+				useUnifiedTopology:true
+});
+
+mongoose.connection.once('open', function(){
+				console.log('Connection Done');
+}).on("error", function (err) {
+			console.log('Connection Error', err);
+});
 const port = process.env.PORT || 3000;
 app.listen(port,function () {
 			console.log(`Server listening to requests on localhost:${port}`);
