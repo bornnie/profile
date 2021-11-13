e18ff194998de05e2dc7e26d3aace02a02789d0f
bornnie <bornwelljames@gmail.com>
2021-11-13 16:47:25 +0200

    database setup

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
-const express = require('expresw');
+const express = require('express');
+const mongoose = require('mongoose')
+require('dotenv').config();
 const app = express();
+
+//DB Setup
+mongoose.connect(process.env.CONN);
+const dbConn = mongoose.connection;
+dbConn.once('open',()=>console.log(`Connection Established`)).on('error', err=>console.log(`Connection Error:${err}`));
 const port = process.env.PORT || 3000;
 app.listen(port,function () {
 			console.log(`Server listening to requests on localhost:${port}`);
